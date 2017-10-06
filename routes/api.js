cc430d2e40c54254ebffc829132b5422132e8185
gjgjwmertens <g.j.g.j.w.mertens@uva.nl>
2017-10-06 21:40:51 +0200

    replaced async test function

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,32 @@
+require('../lib/helpers');
 let express = require('express');
 let bodyParser = require('body-parser');
 let st = require('../lib/server-test');
-require('../lib/helpers');
 let router = express.Router();
 let runStIntVal = null;
+let cc = null;
 let serverList = [
    'AZ005.fmg.uva.nl',
    'webserver13.fmg.uva.nl'
 ];
 
-async function rst(srv) {
-   let result = await st.runServerTest(srv);
-   return 'Server tested: ' + srv + ' with result: ' +  result;
-};
+// async function rst(srv) {
+//    let result = await st.runServerTest(srv);
+//    return 'Server tested: ' + srv + ' with result: ' +  result;
+// };
+
+function testServers() { // TODO add timing
+   serverList.forEach((server) => {
+      cc.send('\n' + new Date().formatedTime() + ' Starting Server test on ' + server);
+      st.runServerTest(server).then((r) => {
+         if (DEBUG) {
+            console.log(r);
+         }
+         cc.send('\n' + new Date().formatedTime() + ' Result from server test: ' + r);
+      });
+      cc.send('\n' + new Date().formatedTime() + ' Server test on ' + server + ' ended.');
+   });
+}
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: false}));
@@ -37,30 +51,23 @@ router.get('/api/:item_id/:new_value', function (req, res) {
 
 router.post('/api', function (req, res) {
    let input = req.body;
-   let cc = req.app.get('cc');
+   if (!cc) {
+      cc = req.app.get('cc');
+   }
 
    switch (input.command) {
       case 'serverTest':
          let t = Date.now();
-         serverList.forEach((server) => {
-            cc.send('\n' + new Date().formatedTime() + ' Starting Server test on ' + server);
-            rst(server).then((r) => {
-               if (DEBUG) {
-                  console.log(r);
-               }
-               cc.send('\n' + new Date().formatedTime() + ' Result from server test: ' + r);
-            });
-            cc.send('\n' + new Date().formatedTime() + ' Server test on ' + server + ' ended.');
-         });
+         testServers();
          t = Date.now() - t;
          res.json({msg: 'Api test ok at: ' + new Date() + ' Done in ' + t});
          break;
       case 'runServerTest':
-         runStIntVal = setInterval(st.runServerTest, 10000);
+         runStIntVal = setInterval(testServers, 10000);
          res.json({msg: 'Server test started at: ' + new Date()});
          break;
       case 'stopServerTest':
-         clearInterval(runIntVal);
+         clearInterval(runStIntVal);
          res.json({msg: 'Server test stopped at: ' + new Date()});
          break;
       default:
