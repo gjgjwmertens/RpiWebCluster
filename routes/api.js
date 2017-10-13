d64d7ccc8db0aaa82478d3ff55b88eeea62c0d38
gjgjwmertens <g.j.g.j.w.mertens@uva.nl>
2017-10-13 21:25:03 +0200

    testing cluster

--- routes/api.js
+++ routes/api.js
@@ -4,12 +4,20 @@ let bodyParser = require('body-parser');
 let st = require('../lib/server-test');
 let router = express.Router();
 let runStIntVal = null;
+let runCtIntVal = null;
 let cc = null;
+// let serverList = [
+//    'webserver1.fmg.uva.nl'
+//    'webserver11.fmg.uva.nl'
+// ];
 let serverList = [
    'AZ005.fmg.uva.nl',
+   'webserver2.fmg.uva.nl',
+   'webserver6.fmg.uva.nl',
    'webserver12.fmg.uva.nl',
    'webserver13.fmg.uva.nl'
 ];
+let cluster = '145.18.151.162';
 
 // async function rst(srv) {
 //    let result = await st.runServerTest(srv);
@@ -36,6 +44,25 @@ function testServers() {
    });
 }
 
+function testCluster() {
+   let server = cluster;
+   let t = [server, Date.now()];
+   cc.send('\n' + new Date().formattedTime() + ' Starting test on ' + server);
+   st.runServerTest(server).then((r) => {
+      if (DEBUG) {
+         console.log(r);
+      }
+      cc.send('\n' + new Date().formattedTime() +
+              ' Result from server test: \r\n             ' + r +
+              '\r\n             ' + t[0] + ' took ' + (Date.now() - t[1]));
+   }, (e) => {
+      cc.send('\n' + new Date().formattedTime() +
+              ' Result from server test: \r\n             ' + e +
+              '\r\n             ' + t[0] + ' took ' + (Date.now() - t[1]));
+   });
+   // cc.send('\n' + new Date().formattedTime() + ' Server test on ' + server + ' ended.');
+}
+
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: false}));
 
@@ -59,13 +86,13 @@ router.get('/api/:item_id/:new_value', function (req, res) {
 
 router.post('/api', function (req, res) {
    let input = req.body;
+   let t = Date.now();
    if (!cc) {
       cc = req.app.get('cc');
    }
 
    switch (input.command) {
       case 'serverTest':
-         let t = Date.now();
          testServers();
          t = Date.now() - t;
          res.json({msg: 'Api test ok at: ' + new Date() + ' Done in ' + t});
@@ -76,6 +103,19 @@ router.post('/api', function (req, res) {
          break;
       case 'stopServerTest':
          clearInterval(runStIntVal);
+         res.json({msg: 'Server test stopped at: ' + new Date()});
+         break;
+      case 'clusterTest':
+         testCluster();
+         t = Date.now() - t;
+         res.json({msg: 'Api test ok at: ' + new Date() + ' Done in ' + t});
+         break;
+      case 'runClusterTest':
+         runCtIntVal = setInterval(testCluster, 10000);
+         res.json({msg: 'Server test started at: ' + new Date()});
+         break;
+      case 'stopClusterTest':
+         clearInterval(runCtIntVal);
          res.json({msg: 'Server test stopped at: ' + new Date()});
          break;
       default:
